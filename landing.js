11e87f058a1a874a477cd32a2b800e13a5e786ff
unknown <pedrohrino13@gmail.com>
2024-04-14 17:19:14 -0300

    login page

---
+++ landing.js
@@ -0,0 +1,5 @@
+// JavaScript para redirecionar quando o botão é clicado
+document.getElementById("botaoCadastro").addEventListener("click", function() {
+    // Redireciona para ../home.html
+    window.location.href = "./login/login.html";
+  });
